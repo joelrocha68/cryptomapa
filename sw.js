755dcd388bc6d924bcd70e140db8625b9c2d8502
Joel Rocha - QA Engineer <86797941+joelrocha68@users.noreply.github.com>
2026-09-22 23:32:31 -0300

    Atualiza cache do PWA para GitHub Pages

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="cryptomapa-v13-1-shell";
-const SHELL=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE="cryptomapa-v13-1-pages";
+const SHELL=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener("fetch",event=>{
